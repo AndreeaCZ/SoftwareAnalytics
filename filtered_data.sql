fa27e442e34f1c60a4d42a3cfa359202146a70c4
Matej Kucera <matej.kucera.01@gmail.com>
2024-03-06 16:43:36 +0100

    do some preliminary tests, wrote results in overleaf

--- filtered_data.sql
+++ filtered_data.sql
@@ -1,13 +1,12 @@
-SELECT SUM(pull_request_count) AS total_pull_requests
-FROM (
-SELECT
-    reponame,
-    COUNT(*) AS pull_request_count
+-- this is how we create the filtered_data table
+
+create table filtered_data as
+SELECT *
 FROM
     new_pullreq
 WHERE
     (src_churn > 5 OR test_churn > 5 OR num_commits > 2) AND
-    mergetime_minutes > 2 AND mergetime_minutes < 24 * 60 * 60 AND
+    mergetime_minutes > 2 AND mergetime_minutes < 60 * 24 * 60 AND
     sloc > 1000 AND
     team_size > 2 AND
     acc_commit_num > 5 AND
@@ -18,11 +17,7 @@ WHERE
     project_age > 90 AND
     description_length > 5 AND
     contrib_country IS NOT NULL AND
+    contrib_country != '' AND
     inte_country IS NOT NULL AND
-    contrib_country != inte_country
-GROUP BY
-    reponame
-HAVING
-    COUNT(*) >= 10
-) AS filtered_results
+    inte_country != ''
 ;
--- main.py
+++ main.py
@@ -1,3 +1,54 @@
+import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import f_oneway
 
 
+conn = sqlite3.connect('db')
 
+c = conn.cursor()
+
+# just does anova on whatever you select
+# c.execute('select mergetime_minutes, same_country from filtered_data')
+#
+# same_country = []
+# different_country = []
+# for row in c:
+#     if row[0] == '':
+#         continue
+#     if row[1] == 1:
+#         same_country.append(float(row[0]))
+#     else:
+#         different_country.append(float(row[0]))
+#
+# c.close()
+#
+# f, p = f_oneway(same_country, different_country)
+# print(f'f: {f}, p: {p}')
+
+
+# RQ2
+c.execute('SELECT fd.mergetime_minutes, nc.num_countries FROM filtered_data fd JOIN (SELECT reponame, COUNT(DISTINCT '
+          'contrib_country)'
+          'AS num_countries FROM filtered_data GROUP BY reponame) nc ON fd.reponame = nc.reponame;')
+data = c.fetchall()
+print(len(data))
+
+c.close()
+
+# correlation
+boxplot_data = []
+for i in range(1, 25):
+    arr = [mergetime for mergetime, num_countries in data if num_countries == i]
+    if len(arr) > 0:
+        boxplot_data.append(arr)
+
+print(f'boxplot_data: {boxplot_data}')
+plt.boxplot(boxplot_data)
+plt.show()
+
+f, p = f_oneway(*boxplot_data)
+
+print(f'f: {f}, p: {p}')
+
+conn.close()
